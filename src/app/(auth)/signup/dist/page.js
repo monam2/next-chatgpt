a3a2eacb67bfd83a9b23a640895c0e2a92aecb90
monam2 <kangcw0107@gmail.com>
2025-01-31 07:43:33 +0900

    회원가입 페이지 추가

---
+++ src/app/(auth)/signup/dist/page.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var SignUpForm_1 = require("@/components/auth/SignUpForm");
+function SignupPage() {
+    return React.createElement(SignUpForm_1["default"], null);
+}
+exports["default"] = SignupPage;
