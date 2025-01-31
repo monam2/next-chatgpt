9fddffeed228facbf27fe2567485739656c266d4
monam2 <kangcw0107@gmail.com>
2025-01-31 22:23:38 +0900

    user 스키마 추가

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,10 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-serverless";
+import { user } from "./schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle(sql);
+const sql = neon(
+  process.env.DATABASE_URL!,
+) as unknown as string;
+const db = drizzle(sql, { schema: { user } });
 
 export default db;
