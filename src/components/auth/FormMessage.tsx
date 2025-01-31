7b2ecd6538dd6d84cc123f075b7878f64f05a07d
monam2 <kangcw0107@gmail.com>
2025-01-31 22:22:22 +0900

    코드 정렬

--- src/components/auth/FormMessage.tsx
+++ src/components/auth/FormMessage.tsx
@@ -4,6 +4,8 @@ type Props = {
 
 export function FormMessage({ message }: Props) {
   return (
-    <p className="text-sm font-medium text-red-500 ml-1 mt-1">{message}</p>
+    <p className="text-sm font-medium text-red-500 ml-1 mt-1">
+      {message}
+    </p>
   );
 }
