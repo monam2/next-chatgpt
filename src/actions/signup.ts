4db4746def5d3c7f896fd9219688c363bafb4315
monam2 <kangcw0107@gmail.com>
2025-02-01 00:01:08 +0900

    signup 액션 리다이렉트 로직 추가

--- src/actions/signup.ts
+++ src/actions/signup.ts
@@ -1,10 +1,14 @@
 "use server";
 
 import { getUserByEmail } from "@/data/user";
+import db from "@/db";
+import { user } from "@/db/schema";
 import { SignUpSchema } from "@/schemas/auth";
+import { hash } from "bcrypt";
+import { redirect } from "next/navigation";
 
 export const signUp = async (
-  _: any,
+  _: unknown,
   formData: FormData,
 ) => {
   // 1. 유효성 검증
@@ -23,15 +27,30 @@ export const signUp = async (
   // 2. 존재하는 사용자인지 체크
   const { name, email, password } = validateFields.data;
 
-  const existingUser = await getUserByEmail(email);
+  try {
+    const existingUser = await getUserByEmail(email);
 
-  if (existingUser) {
-    return {
-      errorMessage: "이미 존재하는 사용자입니다.",
-    };
+    if (existingUser) {
+      return {
+        errorMessage: "이미 존재하는 사용자입니다.",
+      };
+    }
+
+    const hashedPassword = await hash(password, 10);
+
+    // 3. db에 회원 정보 저장
+    await db.insert(user).values({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (error) {
+    console.error(error);
+    return { errorMessage: "문제가 발생했습니다." };
   }
 
-  // 3. db에 회원 정보 저장
+  // 에러가 없으면 리다이렉트
+  redirect("/login");
 
   // 4. 성공 / 실패 처리
 };
