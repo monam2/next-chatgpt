6bcecd6caa465a14e923b23a8b8c7f12fdf22255
monam2 <kangcw0107@gmail.com>
2025-02-17 23:11:20 +0900

    middleware 설정

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,35 @@
+import { verify } from "@/actions/sessions";
+import {
+  AUTH_ROUTES,
+  BASE_URL,
+  PUBLIC_ROUTES,
+} from "@/constants/routes";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(
-    new URL("/home", request.url),
-  );
+export async function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+  const isPublicRoute = PUBLIC_ROUTES.includes(pathname);
+
+  const cookie = (await cookies()).get("session")?.value;
+  const session = await verify(cookie);
+
+  if (!isPublicRoute && !session) {
+    return NextResponse.redirect(
+      new URL(AUTH_ROUTES.LOGIN, request.nextUrl),
+    );
+  }
+
+  if (isPublicRoute && session) {
+    return NextResponse.redirect(
+      new URL(BASE_URL, request.nextUrl),
+    );
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/",
+  matcher:
+    "/((?!api|_next/static|_next/image|favicon.ico).*)",
 };
